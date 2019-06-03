8e1c9bf8baf98be5d56c109cd8861dcf79c587b9
SSE4 <tomskside@gmail.com>
2019-06-03 17:57:17 +0200

    - initial versions, works locally on OSX & Linux
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,73 +1,113 @@
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanException
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
+class GccConan(ConanFile):
+    name = "gcc"
+    version = "9.1.0"
+    description = "The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Ada, Go, " \
+                  "and D, as well as libraries for these languages (libstdc++,...)"
+    topics = ("conan", "gcc", "logging")
+    url = "https://github.com/bincrafters/conan-gcc"
+    homepage = "https://gcc.gnu.org/"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
+    license = "GPL-3.0-or-later"
+    exports = ["LICENSE.md"]
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
-    # Custom attributes for Bincrafters recipe conventions
+    options = {"shared": [True, False], "fPIC": [True, False], "languages": "ANY"}
+    default_options = {"shared": False, "fPIC": True, "languages": "c,c++"}
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
+    no_copy_source = True
 
-    requires = (
-        "OpenSSL/1.0.2r@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
+    requires = ("gmp/6.1.2@bincrafters/stable",
+                "mpfr/4.0.2@bincrafters/stable",
+                "mpc/1.1.0@bincrafters/stable",
+                "zlib/1.2.11@conan/stable")
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
+        # https://gcc.gnu.org/mirrors.html
+        mirrors = ["ftp://ftp.gnu.org/gnu/gcc",
+                   "ftp://ftp.lip6.fr/pub/gcc/releases",
+                   "ftp://ftp.irisa.fr/pub/mirrors/gcc.gnu.org/gcc/releases/",
+                   "ftp://ftp.uvsq.fr/pub/gcc/releases/",
+                   "http://mirrors-usa.go-parts.com/gcc/releases/",
+                   "http://mirrors.concertpass.com/gcc/releases/"]
+        for mirror in mirrors:
+            try:
+                source_url = mirror + "/{n}-{v}/{n}-{v}.tar.gz".format(n=self.name, v=self.version)
+                tools.get(source_url, sha256="be303f7a8292982a35381489f5a9178603cbe9a4715ee4fa4a815d6bcd2b658d")
+                break
+            except ConanException:
+                pass
         extracted_dir = self.name + "-" + self.version
-
-        # Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
-
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        libdir = "%s/lib/gcc/%s" % (self.package_folder, self.version)
+        tools.replace_in_file(os.path.join(self.source_folder,
+                                           self._source_subfolder, "gcc", "config", "i386", "t-linux64"),
+                              "m64=../lib64", "m64=../lib", strict=False)
+        tools.replace_in_file(os.path.join(self.source_folder,
+                                           self._source_subfolder, "libgcc", "config", "t-slibgcc-darwin"),
+                              "@shlib_slibdir@", libdir, strict=False)
+
+        pkgversion = "Conan GCC %s" % self.version
+        tools.mkdir(self._build_subfolder)
+        condigure_dir = os.path.abspath(os.path.join(self.source_folder, self._source_subfolder))
+        with tools.chdir(self._build_subfolder):
+            # http://www.linuxfromscratch.org/lfs/view/development/chapter06/gcc.html
+            args = ["--enable-languages=%s" % self.options.languages,
+                    "--disable-nls",
+                    "--disable-bootstrap",
+                    "--disable-multilib",  # this means building two architectures at once, too hard for now
+                    "--with-system-zlib",
+                    "--program-suffix=-%s" % self.version,
+                    "--with-bugurl=https://github.com/bincrafters/community/issues",
+                    "--with-pkgversion=%s" % pkgversion,
+                    "--libdir=%s" % libdir,
+                    "--with-gmp=%s" % self.deps_cpp_info["gmp"].rootpath,
+                    "--with-mpfr=%s" % self.deps_cpp_info["mpfr"].rootpath,
+                    "--with-mpc=%s" % self.deps_cpp_info["mpc"].rootpath]
+            if self.settings.os == "Macos":
+                # https://github.com/Homebrew/homebrew-core/blob/master/Formula/gcc.rb
+                args.extend(["--with-native-system-header-dir=/usr/include",
+                             "--with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"])
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build.libs = []  # otherwise causes config.log to fail finding -lmpc
+            if self.settings.compiler in ["clang", "apple-clang"]:  # GCC doesn't like Clang-specific flags
+                if self.settings.compiler.libcxx == "libc++":
+                    env_build.cxx_flags.remove("-stdlib=libc++")
+                elif self.settings.compiler.libcxx in ["libstdc++", "libstdc++11"]:
+                    env_build.cxx_flags.remove("-stdlib=libstdc++")
+            env_build.configure(args=args, configure_dir=condigure_dir)
+            make_args = []
+            if self.settings.os == "Macos":
+                #  Ensure correct install names when linking against libgcc_s;
+                #  see discussion in https://github.com/Homebrew/legacy-homebrew/pull/34303
+                make_args.append("BOOT_LDFLAGS=-Wl,-headerpad_max_install_names")
+            env_build.make(args=make_args)
+            env_build.install()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH env var with : " + bindir)
+        self.env_info.PATH.append(bindir)
+
+        cc = os.path.join(bindir, "gcc-%s" % self.version)
+        self.output.info("Appending CC env var with : " + cc)
+        self.env_info.CC = cc
+
+        cxx = os.path.join(bindir, "g++-%s" % self.version)
+        self.output.info("Appending CXX env var with : " + cxx)
+        self.env_info.CXX = cxx
---
+++ test_package/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Bincrafters\n");
+    return 0;
+}
